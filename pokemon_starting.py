08b0cdf197585ba637364789c50203e51d2ac5ad
John Cribb <johncribb1@gmail.com>
2020-04-27 11:34:54 +0100

    Attack function created.  Can probably reduce the coding.

--- pokemon_starting.py
+++ pokemon_starting.py
@@ -10,7 +10,6 @@ class Pokemon:
     def __repr__(self):
         return 'The current Pokemon held is ' + self.name + ', with a type of ' + self.type + '. Their current level is ' + str(self.level) + ' and their current health is ' + str(self.health) + ', out of a maximum health score of ' + str(self.max_health) + '.'
         
-
     def lose_health(self, value):
         if self.is_knocked_out == False:
             self.health = self.health - value
@@ -35,7 +34,6 @@ class Pokemon:
         else:
             print(self.name + ' is already knocked out!')
 
-
     def knocked_out(self):
         if self.health <= 0:
             self.is_knocked_out = True
@@ -43,11 +41,56 @@ class Pokemon:
         else:
             self.is_knocked_out = False
             return False
+    
+    def revive(self):
+        if self.is_knocked_out == True:
+            self.health = self.max_health
+            self.is_knocked_out = False
+            print(self.name + ' has been revived! Take care of him.')
+        else:
+            print(self.name + ' isn\'t knocked out!') 
+
+    def attack(self, recipient, attack_value):
+        if self.type == 'electric':
+            if recipient.type == 'fire':
+                recipient.lose_health(attack_value)
+            elif recipient.type == 'electric':
+                new_attack = (0.5 * attack_value)
+                recipient.lose_health(new_attack)
+            elif recipient.type == 'water':
+                new_attack = (2 * attack_value)
+                recipient.lose_health(new_attack)
+            else:
+                recipient.lose_health(attack_value)
+        elif self.type == 'fire':
+            if recipient.type == 'electric':
+                recipient.lose_health(attack_value)
+            elif recipient.type == 'fire':
+                new_attack = (0.5 * attack_value)
+                recipient.lose_health(new_attack)
+            elif recipient.type == 'water':
+                new_attack = (0.5 * attack_value)
+                recipient.lose_health(new_attack)
+            else:
+                recipient.lose_health(attack_value)
+        elif self.type == 'water':
+            if recipient.type == 'fire':
+                new_attack = (2 * attack_value)
+                recipient.lose_health(new_attack)
+            elif recipient.type == 'electric':
+                recipient.lose_health(attack_value)
+            elif recipient.type == 'water':
+                new_attack = (0.5 * attack_value)
+                recipient.lose_health(new_attack)
+            else:
+                recipient.lose_health(attack_value)
 
 
 
-pikachu = Pokemon('Pikachu', 0, 60, 60, 'lightning', False)
-#print(pikachu)
-pikachu.lose_health(70)
-pikachu.gain_health(10)
-#print(pikachu)
+pikachu = Pokemon('Pikachu', 0, 60, 60, 'electric')
+charmander = Pokemon('Charmander', 0, 50, 50, 'fire')
+squirtle = Pokemon('Squirtle', 0, 60, 60, 'water')
+
+pikachu.attack(squirtle, 50)
+
+#print(squirtle)
